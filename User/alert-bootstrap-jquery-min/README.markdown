7235294b0cb5daa644eef8d9a8061019e7b48fd8
Devansh26 <devansh981226@gmail.com>
2022-04-07 16:02:37 +0530

    Add files via upload

---
+++ User/alert-bootstrap-jquery-min/README.markdown
@@ -0,0 +1,5 @@
+# Alert bootstrap jquery.min
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/SergiuC121/pen/jObBado](https://codepen.io/SergiuC121/pen/jObBado).
+
+
---
+++ User/alert-bootstrap-jquery-min/dist/script.js
@@ -0,0 +1,15 @@
+$('.aaa').click(function() {
+      toastr.success('text text text text text text text text text text text text text text')
+    }); 
+
+    $('.sss').click(function() {
+      toastr.info('text text text text text text text text text text text text text text')
+    });
+
+    $('.ddd').click(function() {
+      toastr.error('text text text text text text text text text text text text text text')
+    });
+
+    $('.fff').click(function() {
+      toastr.warning('text text text text text text text text text text text text text text')
+    });
